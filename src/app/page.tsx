03370c90b525e54aa996c760a671a25743edc07b
Said zain <saidzain897@gmail.com>
2024-07-11 16:02:12 +0300

    add iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,20 @@ export default function Home() {
       <About />
       <Impact />
       <Join />
+      <div className="layout-wrapper pt-32 text-center mx-auto my-12" id="join">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/H1KFZ9p0vkc?si=-V3WLNXeKmOI0td8"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="mx-auto w-full h-96"
+        ></iframe>
+      </div>
+
       <Contact />
       <Footer />
     </main>
